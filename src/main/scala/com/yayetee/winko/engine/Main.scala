9845b12f79f896a86b72a96ae8020f16c4eb9f5b
Tymon Tobolski <i@teamon.eu>
2010-03-03 15:01:48 +0100

    Very basic Main engine class and Processing GUI sceleton

---
+++ src/main/scala/com/yayetee/winko/engine/Main.scala
@@ -0,0 +1,16 @@
+package com.yayetee.winko.engine
+
+import com.yayetee.winko.gui.Processing
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: teamon
+ * Date: 2010-03-03
+ * Time: 14:14:30
+ */
+
+object Main {
+	def main(args: Array[String]){
+		Processing.init
+	}
+}
---
+++ src/main/scala/com/yayetee/winko/gui/Processing.scala
@@ -0,0 +1,82 @@
+package com.yayetee.winko.gui
+
+import processing.core.{PConstants, PApplet}
+import java.awt.event.{WindowEvent, WindowAdapter}
+import javax.swing.JFrame
+
+/**
+ * User: teamon
+ * Date: 2010-03-03
+ * Time: 14:11:22
+ */
+
+/**
+ * Basic GUI object that uses Processing API
+ */
+
+object Processing {
+	def init {
+		val applet = new MainWindow
+		applet.init
+
+		val frame = new JFrame("winko")
+		frame.addWindowListener(new WindowAdapter {
+			override def windowClosing(e: WindowEvent) { System.exit(0) }
+		})
+
+		frame.setVisible(false)
+		frame.getContentPane.removeAll
+		frame.getContentPane.add(applet)
+		frame.pack
+		frame.setVisible(true)
+	}
+
+	def draw {
+
+	}
+}
+
+
+class MainWindow extends PApplet {
+	override def setup {
+		size(400, 300, PConstants.OPENGL)
+
+		smooth
+		noStroke
+
+		rectMode(PConstants.CENTER)
+		ellipseMode(PConstants.CENTER)
+
+		textFont(loadFont("QuicksandBook.vlw"));
+	}
+
+	override def draw {
+		background(51)
+
+		//    ObjectManager.objects.foreach(e => {
+		//      pushMatrix
+		//      pushStyle
+		//      translate(e.x, e.y)
+		//      e.paint(this)
+		//      popMatrix
+		//      popStyle
+		//    })
+		//
+		//    ObjectManager.cursors.foreach(e => {
+		//      pushMatrix
+		//      pushStyle
+		//      translate(e.x, e.y)
+		//      e.paint(this)
+		//      popMatrix
+		//      popStyle
+		//    })
+		//
+		//    ObjectManager.gfxObjects.foreach(e => {
+		//      pushMatrix
+		//      pushStyle
+		//      e.paint(this)
+		//      popMatrix
+		//      popStyle
+		//    })
+	}
+}
